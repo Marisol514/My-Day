9a20a22d53336b26dcc2bc140d8192eb17c3b516
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-01-27 15:37:17 -0800

    updated the JS, added the dinamic time blocks and hours to be displayed from 9 am to 5 PM. Added save button and icon and text area, added a loop to update the hous from AM to PM

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,28 @@
 $(function () {
+  // Function to generate time blocks for specific hours
+  function generateTimeBlocks() {
+    var container = $('#timeBlocks');
+
+    var hoursToDisplay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+    for (var i = 0; i < hoursToDisplay.length; i++) {
+      var hour = hoursToDisplay[i];
+      var ampm = hour >= 12 ? 'PM' : 'AM';
+      var displayHour = hour > 12 ? hour - 12 : hour;
+
+      var timeBlock = $('<div>').addClass('row time-block');
+      var hourLabel = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(displayHour + ampm);
+      var textarea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', 3);
+      var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
+      var saveIcon = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
+
+      saveBtn.append(saveIcon);
+      timeBlock.attr('id', 'hour-' + hour);
+      timeBlock.append(hourLabel, textarea, saveBtn);
+      container.append(timeBlock);
+    }
+  }
+
 
   // Function to display the current date in the header
   function displayCurrentDate() {
