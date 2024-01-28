d1b039e75c5ea73fe09319c8ec85edf8f61e44f4
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-01-27 19:44:26 -0800

    Updated the JavaScript code to update the cod etoo generate the time blocks, display current date on page load as well asupdate the time blocks and apply the past, present and future class load on the page. added the load and save from local storage and added the event listener on the save buttion and the interval fuction to update the time blocks every minute

--- Develop/script.js
+++ Develop/script.js
@@ -23,6 +23,40 @@ $(function () {
     }
   }
 
+  // Function to update time block classes based on current time
+  function updateTimeBlocks() {
+    var currentHour = dayjs().hour();
+
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+      if (blockHour < currentHour) {
+        $(this).removeClass('present future').addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
+  // Function to save user input in local storage
+  function saveToLocalStorage() {
+    $('.time-block').each(function () {
+      var blockId = $(this).attr('id');
+      var userDescription = $(this).find('.description').val();
+      localStorage.setItem(blockId, userDescription);
+    });
+  }
+
+  // Function to load saved data from local storage
+  function loadFromLocalStorage() {
+    $('.time-block').each(function () {
+      var blockId = $(this).attr('id');
+      var storedDescription = localStorage.getItem(blockId);
+      $(this).find('.description').val(storedDescription);
+    });
+  }
 
   // Function to display the current date in the header
   function displayCurrentDate() {
@@ -30,5 +64,25 @@ $(function () {
     $('#currentDay').text(currentDate);
   }
 
+  // Generate time blocks on page load
+  generateTimeBlocks();
 
+  // Display current date on page load
+  displayCurrentDate();
+
+  // Apply past, present, or future class on page load
+  updateTimeBlocks();
+
+  // Load saved data from local storage on page load
+  loadFromLocalStorage();
+
+  // Click event listener for save button
+  $('.saveBtn').on('click', function () {
+    saveToLocalStorage();
+  });
+
+  // Update time blocks every minute
+  setInterval(function () {
+    updateTimeBlocks();
+  }, 60000);
 });
